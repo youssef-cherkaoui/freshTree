a4a2e0f65f88227fb6bb9a4aa76cb157ef1bd011
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:53:46 +0100

    Configuration & Security & JWT

---
+++ src/main/java/com/zalarfresh/ZalarFresh/Configuration/ApplicationConfig.java
@@ -0,0 +1,45 @@
+package com.zalarfresh.ZalarFresh.Configuration;
+
+
+import com.zalarfresh.ZalarFresh.Repository.AdminRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final AdminRepository adminRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return email -> adminRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Email non trouvé"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}
