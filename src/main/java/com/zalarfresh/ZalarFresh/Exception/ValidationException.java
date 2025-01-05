5bb13a76dc21ad88eb4b4e8f68924ede8cf0d293
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:51:28 +0100

    Exceptions

---
+++ src/main/java/com/zalarfresh/ZalarFresh/Exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.zalarfresh.ZalarFresh.Exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
