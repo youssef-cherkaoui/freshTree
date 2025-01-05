0c75ec1804a5f54a9b8967f2225bd86857cc7fba
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:36:35 +0100

    creation du ModelVente

--- src/main/java/com/zalarfresh/ZalarFresh/Model/Vente.java
+++ src/main/java/com/zalarfresh/ZalarFresh/Model/Vente.java
@@ -2,6 +2,10 @@ package com.zalarfresh.ZalarFresh.Model;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -20,14 +24,24 @@ public class Vente {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //@Positive(message = "Unit price must be greater than 0")
+    @Positive(message = "Le prix unitaire doit être supérieur à 00")
     private double PrixUnique;
+
+    @Positive(message = "La quantité doit être supérieur à 00")
     private double quantite;
+
+    @Column(nullable = false )
     private double revenue;
+
+    @NotNull(message = "La date est obligatoire")
+    @FutureOrPresent(message = "La date ne peut pas être dans le passé")
     private LocalDate date;
+
+    @NotBlank(message = "le nom du client est obligatoire")
     private String client;
+
     @OneToOne
-    @JoinColumn(name = "harvest_id")
+    @JoinColumn(name = "recolte_id")
     private Recolte recolte;
 
 }
