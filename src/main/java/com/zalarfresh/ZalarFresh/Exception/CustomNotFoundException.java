7273f8936364fc3126c36b9c19e4ec33f83f3e40
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:51:12 +0100

    Exceptions

---
+++ src/main/java/com/zalarfresh/ZalarFresh/Exception/CustomNotFoundException.java
@@ -0,0 +1,7 @@
+package com.zalarfresh.ZalarFresh.Exception;
+
+public class CustomNotFoundException extends RuntimeException {
+    public CustomNotFoundException(String message) {
+        super(message);
+    }
+}
