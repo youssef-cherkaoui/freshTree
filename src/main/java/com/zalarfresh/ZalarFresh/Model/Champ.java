9431e6093197187eec5216de6ff1f2728969bdac
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:35:37 +0100

    creation du ModelChamp

--- src/main/java/com/zalarfresh/ZalarFresh/Model/Champ.java
+++ src/main/java/com/zalarfresh/ZalarFresh/Model/Champ.java
@@ -2,30 +2,33 @@ package com.zalarfresh.ZalarFresh.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "Champs")
-@Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
+@Getter
+@Setter
 public class Champ {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Le nom du champ ne peut pas être vide")
     private String nom;
 
+    @DecimalMin(value = "0.1", message = "Champ doit être d'au moins 0,1 hectare")
     @Column(nullable = false)
     private Double surface;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "ferme_id", nullable = false)
     @JsonIgnore
     private Ferme ferme;
