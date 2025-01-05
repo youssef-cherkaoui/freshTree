401af5f38a4d2301a23ddcfe4da7b4b33c2e50c4
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:55:42 +0100

    Repository : admin

---
+++ src/main/java/com/zalarfresh/ZalarFresh/Repository/AdminRepository.java
@@ -0,0 +1,13 @@
+package com.zalarfresh.ZalarFresh.Repository;
+
+import com.zalarfresh.ZalarFresh.Model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+
+    Optional<Admin> findByEmail(String email);
+}
