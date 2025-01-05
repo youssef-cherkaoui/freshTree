6298b6d5cbeccd28782c9ca39742eba5498a8439
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-05 23:36:17 +0100

    creation du ModelFerme

--- src/main/java/com/zalarfresh/ZalarFresh/Model/Ferme.java
+++ src/main/java/com/zalarfresh/ZalarFresh/Model/Ferme.java
@@ -1,35 +1,43 @@
 package com.zalarfresh.ZalarFresh.Model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Entity
-@Table(name = "Ferme")
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class Ferme {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
+    @NotBlank(message = "Le nom de la ferme ne peut pas être vide")
     private String nom;
-    private String localization;
 
+    @NotBlank(message = "Localisation ne peut pas être vide")
+    private String localisation;
+
+    @DecimalMin(value = "0.1", message = "La surface de l'exploitation doit être supérieure à 0,1 hectare")
     @Column(nullable = false)
     private double surface;
 
+    @PastOrPresent(message ="La date de création doit être dans le passé ou le présent" )
     @Column(nullable = false)
     private LocalDate creationDate;
 
     @OneToMany(mappedBy = "ferme", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
-    private List<Champ> champs;
+    @JsonIgnore
+    private List<Champ> champ;
 }
