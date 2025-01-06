ad2dab034b87e8d9f7d574683a4f24ff8ac26ece
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-06 13:57:46 +0100

    ApplicationRun

---
+++ src/main/java/com/zalarfresh/ZalarFresh/ZalarFreshApplication.java
@@ -0,0 +1,13 @@
+package com.zalarfresh.ZalarFresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZalarFreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZalarFreshApplication.class, args);
+	}
+
+}
