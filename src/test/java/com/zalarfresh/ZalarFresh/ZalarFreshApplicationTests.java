0e8909fe4ad2ea0700424b9d5072d40d08e1d9e2
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-06 16:11:39 +0100

    Appllication test

---
+++ src/test/java/com/zalarfresh/ZalarFresh/ZalarFreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalarfresh.ZalarFresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZalarFreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
