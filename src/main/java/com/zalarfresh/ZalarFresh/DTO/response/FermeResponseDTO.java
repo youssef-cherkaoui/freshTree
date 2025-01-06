8e7f5a5655acb5c4a672e1cae5e7d660d7aec2bb
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-06 16:13:13 +0100

    ResponseDTO : ferme

--- src/main/java/com/zalarfresh/ZalarFresh/DTO/response/FermeResponseDTO.java
+++ src/main/java/com/zalarfresh/ZalarFresh/DTO/response/FermeResponseDTO.java
@@ -2,6 +2,7 @@ package com.zalarfresh.ZalarFresh.DTO.response;
 
 import com.zalarfresh.ZalarFresh.DTO.embarquee.ChampDTO;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public record FermeResponseDTO(
@@ -9,6 +10,7 @@ public record FermeResponseDTO(
         String nom,
         String localisation,
         Double surface,
+        LocalDate creationDate,
         List<ChampDTO> champ
 ) {
 }
