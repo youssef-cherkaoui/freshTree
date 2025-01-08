fe9149aa61b6e246ed3885e8e804d3e962170f36
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2025-01-08 02:31:32 +0100

    DTO : RecolteResponseDTO

--- src/main/java/com/zalarfresh/ZalarFresh/DTO/response/RecolteResponseDTO.java
+++ src/main/java/com/zalarfresh/ZalarFresh/DTO/response/RecolteResponseDTO.java
@@ -1,11 +1,12 @@
 package com.zalarfresh.ZalarFresh.DTO.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.zalarfresh.ZalarFresh.Model.Saison;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record RecolteResponseDTO(
         Long id,
-        String season,
+        Saison season,
         Double quantity,
         ChampResponseDTO champ,
         ArbreResponseDTO arbre
